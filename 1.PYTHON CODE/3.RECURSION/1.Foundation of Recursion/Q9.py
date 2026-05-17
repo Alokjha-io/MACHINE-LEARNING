382dcc64bd3a63e81cfece0a86c7a34a19f5db31
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-05-17 20:43:54 +0530

    Add files via upload

---
+++ 1.PYTHON CODE/3.RECURSION/1.Foundation of Recursion/Q9.py
@@ -0,0 +1,14 @@
+'''
+Print Fibonacci series up to n terms recursively. 
+'''
+
+n = int(input("Enter number : "))
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+for i in range(n):
+    print(fibonacci(i))
