bd6b148e94e769c4fffdf476863634e754d12e53
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-05-17 20:42:58 +0530

    Add files via upload

---
+++ 1.PYTHON CODE/3.RECURSION/1.Foundation of Recursion/Q4.py
@@ -0,0 +1,12 @@
+'''
+Print only odd numbers from 1 to n recursively.
+'''
+
+n = int(input("Enter n : "))
+def evennum(i,n):
+    if i>n:
+        return
+    if i%2!=0:
+        print(i)
+    evennum(i+1,n)
+evennum(1,n)
