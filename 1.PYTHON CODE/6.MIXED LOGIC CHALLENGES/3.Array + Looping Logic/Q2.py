01f691fa30b37afb6d71121b7a556347736e995f
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-09-02 19:16:56 +0530

    Add files via upload

--- 1.PYTHON CODE/6.MIXED LOGIC CHALLENGES/3.Array + Looping Logic/Q2.py
+++ 1.PYTHON CODE/6.MIXED LOGIC CHALLENGES/3.Array + Looping Logic/Q2.py
@@ -1,12 +1,24 @@
 '''
-Count vowels in each word of a sentence.
+Count how many positive, negative, and zero elements are in an array. 
 '''
 
-sen = input("Enter a sentence : ")
-words = sen.split()
-for i in range(len(words)):
-    count = 0
-    for v in words[i]:
-        if v in ['a','e','i','o','u','A','E','I','O','U']:
-            count+=1
-    print("Count of vowels in",words[i],count)
+arr = []
+n = int(input("Enter size of array : "))
+for i in range(n):
+    arr.append(int(input("Enter Element : ")))
+
+p_count = 0
+n_count = 0
+z_count = 0
+
+for a in arr:
+    if a>0:
+        p_count+=1
+    elif a<0:
+        n_count+=1
+    else:
+        z_count+=1
+print("Positive Element count : ",p_count)
+print("Negative Element count : ",n_count)
+print("Zero Element count : ",z_count)
+
